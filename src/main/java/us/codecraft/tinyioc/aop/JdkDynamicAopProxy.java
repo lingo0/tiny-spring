a9004b53bf094376ec579c00ebb19c1e04df7d3c
lingo0 <315914415@qq.com>
2018-04-25 12:11:15 +0800

    AOP分析

--- src/main/java/us/codecraft/tinyioc/aop/JdkDynamicAopProxy.java
+++ src/main/java/us/codecraft/tinyioc/aop/JdkDynamicAopProxy.java
@@ -8,20 +8,48 @@ import java.lang.reflect.Proxy;
 
 /**
  * 基于jdk的动态代理
- * 
+ *
  * @author yihua.huang@dianping.com
  */
+
+/**
+ * 假设以下情况：
+ * 对象 obj 实现了 IObj 接口，接口中有一个方法func（Object[] args）
+ * 对象 handler 是 InvocationHandler 的实例。
+ * 那么，通过 Proxy 的 newProxyInstance 可以返回 obj 的代理对象 proxy。
+ * <p>
+ * 当调用 proxy.func(args)时，对象内部将委托给 handler.invoke(proxy, func, args) 函数实现。
+ * 因此，在 handler 的 invoke 中，可以完全对方法拦截的处理。
+ * 可以先判断是不是要拦截的方法，如果是，进行拦截 比如先做一些操作，在调用原来的方法，对应了Spring的前置通知；
+ * 如果不是，则调用原来的方法。
+ */
+
 public class JdkDynamicAopProxy extends AbstractAopProxy implements InvocationHandler {
 
-    public JdkDynamicAopProxy(AdvisedSupport advised) {
-        super(advised);
-    }
+	public JdkDynamicAopProxy(AdvisedSupport advised) {
+		super(advised);
+	}
 
 	@Override
 	public Object getProxy() {
+
+		/**
+		 * Proxy 来自 JDK API。提供生成对象的动态代理功能，
+		 * 通过Object newProxyInstance(ClassLoader loader,Class<?>[] interfaces,InvocationHandler h) 方法返回一个代理对象
+		 */
 		return Proxy.newProxyInstance(getClass().getClassLoader(), advised.getTargetSource().getInterfaces(), this);
 	}
 
+	/**
+	 * InvocationHandler 来自 JDK API。
+	 * 通过 Object invoke(Object proxy, Method method,Object[] args) 方法实现代理对象中方法的调用和其他处理。
+	 *
+	 * @param proxy
+	 * @param method
+	 * @param args
+	 * @return
+	 * @throws Throwable
+	 */
 	@Override
 	public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
 		MethodInterceptor methodInterceptor = advised.getMethodInterceptor();
@@ -33,5 +61,6 @@ public class JdkDynamicAopProxy extends AbstractAopProxy implements InvocationHa
 			return method.invoke(advised.getTargetSource().getTarget(), args);
 		}
 	}
+
 
 }
--- src/main/java/us/codecraft/tinyioc/aop/Pointcut.java
+++ src/main/java/us/codecraft/tinyioc/aop/Pointcut.java
@@ -1,6 +1,9 @@
 package us.codecraft.tinyioc.aop;
 
 /**
+ * 切点对象可以获取一个 ClassFilter 对象和一个 MethodMatcher 对象。
+ * 前者用于判断是否对某个对象进行拦截。（用于筛选代理的目标对象）
+ * 后者用于判读是否对某个对象方法信息拦截（用于 在代理对象中对不同的方法进行不同的操作）
  * @author yihua.huang@dianping.com
  */
 public interface Pointcut {
