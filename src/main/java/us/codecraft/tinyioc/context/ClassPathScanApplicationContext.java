edc79892b7f7a1b6be87c53f57428a7900c13641
lingo0 <315914415@qq.com>
2018-04-28 11:19:22 +0800

    IOc

--- src/main/java/us/codecraft/tinyioc/context/ClassPathScanApplicationContext.java
+++ src/main/java/us/codecraft/tinyioc/context/ClassPathScanApplicationContext.java
@@ -49,7 +49,7 @@ public class ClassPathScanApplicationContext extends AbstractApplicationContext 
 			beanDefinition.setBeanClassName(className);
 			beanDefinition.setBeanClass(cl);
 
-			// 注入属性
+			// 注入属性, 使用@Resource注解
 			Field[] beanFields = cl.getDeclaredFields();
 			for (Field beanField : beanFields) {
 				if (beanField.isAnnotationPresent(Resource.class)) {
